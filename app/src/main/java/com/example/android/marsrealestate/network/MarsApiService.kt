7e9e0102038ca34a982dbe00ce53e52f985c8d6e
Praveen <pkchmits@gmail.com>
2020-12-27 06:33:07 +0530

    Called retrofit APi successfully with ScalarConvertor for String response

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,8 +17,28 @@
 
 package com.example.android.marsrealestate.network
 
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
 private const val BASE_URL = "https://mars.udacity.com/"
 
-// TODO (02) Use Retrofit Builder with ScalarsConverterFactory and BASE_URL
-// TODO (03) Implement the MarsApiService interface with @GET getProperties returning a String
-// TODO (04) Create the MarsApi object using Retrofit to implement the MarsApiService
+private val retrofit=Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .build()
+// DONE (02) Use Retrofit Builder with ScalarsConverterFactory and BASE_URL
+interface MarsApiService {
+    @GET("realestate")
+    fun getProperties(): Call<String>
+}
+
+object MarsApi{
+    val retrofitService:MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+
+}
+// DONE (03) Implement the MarsApiService interface with @GET getProperties returning a String
+// DONE (04) Create the MarsApi object using Retrofit to implement the MarsApiService
